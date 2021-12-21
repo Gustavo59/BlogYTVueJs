248e74779b90a4b23e7686e583700040f6cee7f5
Gustavo-barros-shopper <gustavo.barros@shopper.com.br>
2021-12-21 17:56:45 -0300

    Pagina para visualização dos blogs

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="blog-card">
-    <div class="icons">
+    <div v-show="editPost" class="icons">
       <div class="icon">
         <Edit class="edit" />
       </div>
@@ -30,6 +30,11 @@ export default {
   name: "blogCard",
   props: ["post"],
   components: { Arrow, Edit, Delete },
+  computed: {
+    editPost() {
+      return this.$store.state.editPost;
+    },
+  },
 };
 </script>
 
